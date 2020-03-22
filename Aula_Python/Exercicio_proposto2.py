f2341d2c3651fac21f687c1fa2b075f6b302b42b
carlos <gabrielcarlos331@gmail.com>
2020-03-22 04:00:19 -0300

    Exercicio proposto

---
+++ Aula_Python/Exercicio_proposto2.py
@@ -0,0 +1,8 @@
+#     Soma de Duas listas           #
+'''l1 = [1, 2, 3, 4, 5, 6, 7]       #
+l2 = [1, 2, 3, 4]                   #
+l3 = [x+y  for x, y in zip(l1,l2)]  #
+                                    #
+print(l3)          '''              #
+#       List coprehesion + Zip      #
+#####################################
