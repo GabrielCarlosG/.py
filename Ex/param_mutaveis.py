08aad2dcd1fb5a2c5642f37601e4525b5125c231
carlos <gabrielcarlos331@gmail.com>
2020-03-30 23:54:33 -0300

    aula 84

---
+++ Ex/param_mutaveis.py
@@ -0,0 +1,19 @@
+# MutÃ¡vel: Listas, dicionÃ¡rios
+# ImutÃ¡vel: Tuplas, strings, nÃºmeros, True, False, None
+
+
+def lista_de_clientes(clientes_iteravel, lista=None):
+    if lista is None:
+        lista = []
+    lista.extend(clientes_iteravel)
+    return lista
+
+
+lista_clientes_1 = ['Fabricio']
+clientes1 = lista_de_clientes(['JoÃ£o', 'Maria', 'Eduardo'], lista_clientes_1)
+clientes2 = lista_de_clientes(['Marcos', 'Jonas', 'Zico'])
+clientes3 = lista_de_clientes(['JosÃ©'])
+
+print(clientes1)
+print(clientes2)
+print(clientes3)
