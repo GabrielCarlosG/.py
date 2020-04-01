da6592cdae132414fe17ed58b07b24bf9ae14ed5
carlos <gabrielcarlos331@gmail.com>
2020-03-31 21:36:51 -0300

    REFACT aula 86

--- Ex/Teste_desafio_lista/funcao.py
+++ Ex/Teste_desafio_lista/funcao.py
@@ -1,12 +1,29 @@
-def inserir(lista, *args):
-    lista.append(*args)
+def inserir(lista_de_tarefas, *args):
+    lista_de_tarefas.append(*args)
+    print()
 
-def ler(lista):
-    print(lista)
+def ler(lista_de_tarefas):
+    print()
+    print('Tarefas: ')
+    for i in lista_de_tarefas:
+        print(f'    {i}')
+    print()
 
-def desfazer(lista):
-    return lista[-1]
+def desfazer(lista_de_tarefas, desfazer):
+    if not lista_de_tarefas:
+        print('Nada a desfazer ')
+        print()
+        return    
+    r_defazer = lista_de_tarefas.pop()
+    desfazer.append(r_defazer)
     
 
-def refazer(lista1, lista2):
-    lista2.append(lista1[-1])
+def refazer(desfazer, lista_de_tarefas):
+    if not desfazer:
+        print('Nada para refazer ')
+        print()
+        return
+    
+    last_refaz = desfazer.pop()
+    lista_de_tarefas.append(last_refaz)
+    
--- Ex/Teste_desafio_lista/tarefas.py
+++ Ex/Teste_desafio_lista/tarefas.py
@@ -1,16 +1,17 @@
 import funcao
-lista_de_tarefas = []
+lista_de_tarefas = ['do', 'dudu', 'edu']
 desfazer = []
-refazer = []
 
 sair = 'N'
 
 while sair == 'N':
     print('#' * 30)
     print('DIGITE:')
-    print('[I] para inserir uma tarefa, \n[V] para ver suas tarefas \n[D] para desfazer \n[R] para refazer')    
+    print('[I] para inserir uma tarefa, \n[V] para ver suas tarefas '
+    '\n[D] para desfazer \n[R] para refazer \n[S] para sair')    
     print('#' * 30)
     acao = input('\nDigite uma das opções acima: ').upper()
+    print()
 
     if acao == 'I':
         tarefa = input('Digite a tarefa que você deseja adicionar: ')
@@ -18,14 +19,14 @@ while sair == 'N':
     elif acao == 'V':
         funcao.ler(lista_de_tarefas)
     elif acao == 'D':
-       d = funcao.desfazer(lista_de_tarefas)
-       desfazer.append(d)
-       lista_de_tarefas.pop()
-       funcao.ler(lista_de_tarefas)
-    #    funcao.ler(desfazer) serve para ver se estava funcionando
+        funcao.desfazer(lista_de_tarefas, desfazer)
+        # funcao.ler(desfazer) 
+        #serve para ver se estava 'funcionando'
     elif acao == 'R':
         funcao.refazer(desfazer, lista_de_tarefas)
+    elif acao == 'S':
+        break
     else:
         print('Digite uma opção válida')
+        continue
 
-    sair = input('Você deseja sair? (S/N)').upper()
